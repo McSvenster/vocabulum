b9d3c5d30ad6e8ca47f80954584f70bf525c52eb
Sven Koesling <sven.koesling@library.ethz.ch>
2018-03-21 16:37:22 +0100

    erste Funktionen

--- vocabulum.rb
+++ vocabulum.rb
@@ -1,6 +1,8 @@
 #! /usr/bin/env ruby
 
 class Verbum
+  attr_reader :uebersetzungen, :treffsicherheit
+
   def initialize(id, uebersetzungen, treffsicherheit)
     @id = id
     @uebersetzungen = uebersetzungen
@@ -9,23 +11,24 @@ class Verbum
 end
 
 class Corpus
+  attr_reader :wortwolke, :sprachen
+
   def initialize(vondatei)
     @lima = Lima.new(vondatei)
-    @wortwolke = []
-    @sprachen = []
-  end
-
-  def wortwolke_bilden()  
+    @wortwolke = {}
     @sprachen = @lima.sprachen
 
+    # wortwolke_bilden
     @lima.dateiinhalt.each do |elementi|
-      @wortwolke << verbum.new(elementi[0], elementi[1..-2], elementi[-1])
+      @wortwolke[elementi[0].to_i] = Verbum.new(elementi[0], elementi[1..-2], elementi[-1])
     end
-
   end
+
 end
 
 class Lima
+  attr_reader :sprachen, :dateiinhalt
+
   def initialize(dateiname)
     @dateiname = dateiname
     @sprachen = []
@@ -52,3 +55,28 @@ class Lima
     
   end
 end
+
+class Trainer
+
+  def initialize(corpus,ausgangssprache,uebersetzungssprache,mixed=false)
+    @corpus = corpus
+    @ausgangssprache = ausgangssprache
+    @uebersetzungsprache = uebersetzungssprache
+    @mixed = mixed
+
+  end
+
+  def aenigma(id)
+    verbum = @corpus.wortwolke[id]
+    frage = verbum.uebersetzungen[@corpus.sprachen.index(@ausgangssprache)]
+    antwort = verbum.uebersetzungen[@corpus.sprachen.index(@uebersetzungsprache)]
+    return frage, antwort
+  end
+
+end
+
+puts "Dann wollen wir mal..."
+corpus = Corpus.new("verbi.csv")
+trainer = Trainer.new(corpus, "de", "lat")
+id = rand(1..3)
+puts "#{trainer.aenigma(id).first} : #{trainer.aenigma(id).last}"
